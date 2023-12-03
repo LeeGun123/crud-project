34965ef402ed03c16029eaee068f58af76b38bb9
Ggyeon <okokh327@gmail.com>
2023-12-03 20:57:35 +0900

    CRUD1203

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,6 @@ public class Main {
             System.out.println("2. 글목록");
             System.out.println("3. 종료");
 
-            System.out.println("test");
 
             int input = scanner.nextInt();
 
