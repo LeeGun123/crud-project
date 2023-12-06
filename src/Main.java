8b42b9ab35e7694d066c2fc6861c9b71af142305
edugun <dlrjs1573@naver.com>
2023-12-06 13:50:12 +0900

    create,update

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,78 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.*;
+
 public class Main {
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // CRUD crud = new CRUD();
+        // Create create = new Create();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        ArrayList<HashMap<String, Object>> table = new ArrayList<>();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        while(true) {
+            System.out.println("원하는 작업을 선택해주세요.");
+            System.out.println("1. 글작성");
+            System.out.println("2. 글목록");
+            System.out.println("3. 종료");
+
+            Scanner scanner = new Scanner(System.in);
+            int input = scanner.nextInt();
+            scanner.nextLine();
+            switch(input) {
+                case 1: // create 부분
+                    HashMap<String, Object> map = new HashMap<>();
+                    System.out.println("제목을 입력하세요 ");
+                    map.put("제목", scanner.nextLine());
+                    System.out.println("내용을 입력하세요 ");
+                    map.put("내용", scanner.nextLine());
+                    table.add(map);
+
+                    break;
+
+                case 2:
+                    System.out.println("글 목록을 출력합니다.");
+                    for (int i = 0; i < table.size(); i++){
+                        HashMap<String,Object> hashMap = table.get(i);
+                        System.out.println(hashMap.get("제목") + " " + hashMap.get("내용"));
+                    }
+
+                    System.out.println("원하는 작업을 선택해주세요.");
+                    System.out.println("1. 글수정");
+                    System.out.println("2. 글삭제");
+                    System.out.println("999. 돌아가기");
+
+                    input = scanner.nextInt();
+                    switch(input) {
+                        case 1: // 수정
+                            System.out.println("수정할 글 번호를 입력해주세요.");
+                            input = scanner.nextInt();
+                            scanner.nextLine();
+                            HashMap<String, Object> update = new HashMap<>();
+                            System.out.println("제목을 입력하세요 ");
+                            update.put("제목", scanner.nextLine());
+                            System.out.println("내용을 입력하세요 ");
+                            update.put("내용", scanner.nextLine());
+                            table.set(input,update);
+                            break;
+
+                        case 2: // 삭제
+                            System.out.println("삭제할 글 번호를 입력해주세요.");
+                            input = scanner.nextInt();
+                            scanner.nextLine();
+                            table.remove(input);
+                            break;
+
+                        case 999:
+                            continue;
+                    }
+                    break;
+                case 3:
+                    System.out.println("프로그램을 종료합니다.");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
+                    break;
+            }
         }
-        System.out.println("test");
-        System.out.println("test2");
-
     }
 }
